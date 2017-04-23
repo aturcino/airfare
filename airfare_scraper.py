4666226ef53eeeabf7b7fb31ce3e19aac1dc2017
aturcino <adnan.turcinovic@gmail.com>
2017-04-23 23:51:27 +0200

    add cluster for price distribution

--- airfare_scraper.py
+++ airfare_scraper.py
@@ -23,14 +23,37 @@ def find_flights():
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, " span.OMOBOQD-u-b")))
 
     s = BeautifulSoup(driver.page_source, "lxml")
-    best_price = s.findAll('span', 'OMOBOQD-mb-p')
 
+    best_price = s.findAll('span', 'OMOBOQD-mb-p')
+    pph = np.array(best_price)/np.array(best_height)
     cities = s.findAll('span', 'OMOBOQD-mb-u')
 
     hlist = []
+    for bar in cities[0]\
+        .findAll('span', 'OMOBOQD-mb-v'):
+        hlist.append(float(bar['style']\
+            .split('height: ')[1].replace('px;',''))*pph)
+
     fares = pd.DataFrame(hlist, columns=['price'])
     px = [for x in fares['price']]
     ff = pd.DataFrame(px, columns=['fare']).reset_index()
 
     X = StandardScaler().fit_transform(ff)
     db = DBSCAN(eps=5, min_samples=1).fit(X)
+
+    labels = db.labels_
+    clusters = len(set(labels))
+
+    pf = pd.concat([ff, pd.DataFrame(db.labels_, columns=['cluster'])], axis=1)
+    rf = pf.groupby('cluster')['fare']\
+           .agg(['min', 'count']).sort_values('min', ascending=True)
+
+    if clusters > 1\
+        and ff['fare'].min() == rf.iloc[0]['min']\
+        and rf.iloc[0]['count'] < rf['count'].quantile(.10)\
+        and rf.iloc[0].['fare'] + 100 < rf.iloc[1]['fare']:
+            city = s.find('span', 'OMOBOQD-mb-p').text
+            fare = s.find('span', 'OMOBOQD-mb-u').text
+
+    else:
+        print("No alert triggered.")
